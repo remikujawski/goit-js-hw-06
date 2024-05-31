97cf3769bf93298e1b4f466b30cdc62fa3085100
Remi Kujawski <remi.kujawski@gmail.com>
2024-05-31 15:41:59 +0200

    Task 2 completed.

--- task-2.js
+++ task-2.js
@@ -1,1 +1,31 @@
 "use strict";
+
+class Storage {
+  #items = [];
+
+  constructor(props) {
+    this.#items = props;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    this.#items.splice(this.#items.indexOf(itemToRemove), 1);
+  }
+}
+
+console.log("");
+console.log("Task 2:");
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
+storage.addItem("Droid");
+console.log(storage.getItems());
+storage.removeItem("Prolonger");
+console.log(storage.getItems());
